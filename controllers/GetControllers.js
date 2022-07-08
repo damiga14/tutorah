d8376c1d5dea701367afe274d112b171070b4d67
damiga14 <davidamiga14@gmail.com>
2022-07-08 12:43:50 -0500

    deploy heroku

--- controllers/GetControllers.js
+++ controllers/GetControllers.js
@@ -5,9 +5,9 @@ module.exports = {
         axios.get('https://api.tutorah.tv/v1/speakers?section=1', { headers: { "Authorization": `19ac3586f1722e4b42b2b821d4b4db4b` } })
             .then((response) => { 
                 let result = response.data
-                
-                res.status(200).json({ message: 'Jajamim loaded', data: result }) 
+
+                res.status(200).json({ message: 'Oradores loaded!', data: result }) 
             })
-            .catch((err) => {console.log(err); res.status(400).json({ message: 'I am Error', data: err }) })
+            .catch((err) => {console.log(err); res.status(400).json({ message: 'I am Error / Oradores not loaded', data: err }) })
     }
 }
